5859a4c8dfe70b049d69578e840476e7f064e2cc
PIERRE Jean-Ernsso <ernsso@hotmail.fr>
2014-05-06 15:59:40 +0200

    path to index

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ var fs = require('fs');
 
 // Chargement du fichier index.html affiché au client
 var server = http.createServer(function(req, res) {
-    fs.readFile('../index.html', 'utf-8', function(error, content) {
+    fs.readFile('../Client/index.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
